54c0c2113247e8f67d9205bd80c458575c578df8
melch <clement.passet@gmail.com>
2021-07-21 15:03:51 +0200

    Suppression d'une sauce

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/Sauce');
+const fs = require('fs');
 
 exports.addSauce = (req, res, next) => {
     const sauce = new Sauce({
@@ -16,7 +17,16 @@ exports.updateSauce = (req, res, next) => {
 };
 
 exports.deleteSauce = (req, res, next) => {
-
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            const filename = sauce.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+                Sauce.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: "Sauce supprimée !" }))
+                    .catch(error => res.status(400).json({ error }));
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 exports.getOneSauce = (req, res, next) => {
