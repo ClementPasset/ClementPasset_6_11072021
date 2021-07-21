7997b543a0a7e72b86f6057e4368c2fb6439f215
melch <clement.passet@gmail.com>
2021-07-21 14:55:46 +0200

    affichage des sauces (routes GET)

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -20,9 +20,13 @@ exports.deleteSauce = (req, res, next) => {
 };
 
 exports.getOneSauce = (req, res, next) => {
-
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(500).json({ error }));
 };
 
 exports.getAllSauces = (req, res, next) => {
-
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(500).json({ error }));
 };
